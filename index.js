4c2158baae692e9994b5a597d129442731bf7a8e
Sweetypatel13 <patelsweety1304@gmail.com>
2026-04-01 19:19:18 +0530

    count odd number

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function printOdd(arr) {
   }
 }
 printOdd(arr);
+
+//count odd numbers:
+let arr1 = [1, 2, 3, 4, 5, 6];
+function countOdd(arr1) {
+  let count = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] % 2 !== 0) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log('Count Odd:', countOdd(arr1));
